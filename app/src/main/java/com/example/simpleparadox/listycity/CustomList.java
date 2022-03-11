49872f5f08345e57c752190dba86545da77243e3
Sean Meyers <smmeyers@ualberta.ca>
2022-03-11 15:31:46 -0700

    fail hasCity test

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -64,4 +64,7 @@ public class CustomList extends ArrayAdapter<City> {
         return;
     }
 
+    //public boolean hasCity(City city) {
+    //    return;// cities.contains(city);
+    //}
 }
--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
 
@@ -14,10 +17,30 @@ public class CustomListTest {
         list = new CustomList(null, new ArrayList<City>());
     }
 
+    private CustomList mockCityList() {
+        CustomList cityList = new CustomList(null, new ArrayList<City>());
+        cityList.add(mockCity());
+        return cityList;
+    }
+
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
     @Test
     public void addCityTest() {
         int listSize = list.getCount();
         list.addCity(new City("Edmonton", "AB"));
         assertEquals(list.getCount(), listSize+1);
+    }
+
+    @Test
+    void testHasCity() {
+        CustomList cityList = mockCityList();
+
+        City city = new City("hello", "world");
+        assertFalse(cityList.hasCity(city));
+        cityList.add(city);
+        assertTrue(cityList.hasCity(city));
     }
 }
